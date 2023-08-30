21c28576d697649a2a8f60ba594cb5892028597f
nt0175083 <nt0175083@lpssonline.com>
2023-08-30 16:02:33 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -51,20 +51,31 @@ dataShapes.push(shape)
     if (currentShape.color = "red") {
       currentShape.goodBehavior = "bounce"
       
-    } else if ()
-  }
+    } else if (currentShape.color = "blue") {
+  currentShape.goodBehavior = "blink"
 
-  color = "red"
-  goodBehavior = "bounce"
+  } else {
+    currentShape.goodBehavior = "spin"
+
     
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood (color, shape, repeat){
+  setBackgroundWithSimple(color, shape, repeat);
+  animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat){
+    repeat += 1;
+    setBackgroundWithMixed (data, repeat);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -72,17 +83,21 @@ dataShapes.push(shape)
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleStatic(currentShape);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
